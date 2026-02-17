6ce34649a7d403ee4861e890e7ad34e8d0ba42b3
Niclas Overby <niclas@overby.me>
2026-02-17 18:32:59 +0100

    chore(rust/systemd): Remove test scripts and unit files

--- build_all.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-cargo build --workspace
-# cargo build --workspace --features linux_eventfd
-# cargo build --workspace --target x86_64-unknown-redox
-
-cd test_c_service
-gcc -o test_service test_service.c $(pkg-config libsystemd --libs)
--- test_scenario/run_scenario.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-# This should print the output of the services ordered by the first number
-../target/debug/systemd-rs 2> /dev/null | grep ".service]" &
-
-sleep 5
-killall systemd-rs
-sleep 1
