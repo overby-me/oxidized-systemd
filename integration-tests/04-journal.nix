b3134b32051185db045397578b5f531613d6cdea
Niclas Overby <niclas@overby.me>
2026-04-07 06:41:42 +0200

    fix(rust/systemd): Remove FDSTORE and machined patchScript workarounds for TEST-04-JOURNAL
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- integration-tests/04-journal.nix
+++ integration-tests/04-journal.nix
@@ -5,15 +5,5 @@
   # - journal-gatewayd: uses C systemd-journal-gatewayd HTTP server (not reimplemented)
   # - journal-remote: uses C systemd-journal-remote/upload (not reimplemented)
   patchScript = ''
-    # Skip FDSTORE stream persistence tests (FDSTORE not implemented).
-    # These tests restart/kill journald and check streams survive; skip both blocks
-    # (lines from "Don't lose streams" through second "systemctl stop forever-print-hola").
-    sed -i '/^# Don.t lose streams on restart/,/^# https:.*\/issues\/15528/{/^# https:/!s/.*/: # SKIP FDSTORE/}' TEST-04-JOURNAL.journal.sh
-
-    # Skip systemd-run --user --machine (requires systemd-machined, not reimplemented).
-    sed -i 's|^systemd-run --user --machine.*|: # SKIP machined not available|' TEST-04-JOURNAL.bsod.sh
-
-
-
   '';
 }
