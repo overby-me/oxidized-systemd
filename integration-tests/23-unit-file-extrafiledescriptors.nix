a4b9786babfe433ed3f56b0b27959f94df8f072f
Niclas Overby <niclas@overby.me>
2026-04-15 06:43:37 +0200

    feat(rust/systemd): Add missing 23-unit-file subtest configs
    Signed-off-by: Niclas Overby <niclas@overby.me>

---
+++ integration-tests/23-unit-file-extrafiledescriptors.nix
@@ -0,0 +1,6 @@
+{
+  name = "23-UNIT-FILE";
+  testEnv = {
+    TEST_MATCH_SUBTEST = "\\.ExtraFileDescriptors\\.sh$";
+  };
+}
---
+++ integration-tests/23-unit-file-verify-unit-files.nix
@@ -0,0 +1,6 @@
+{
+  name = "23-UNIT-FILE";
+  testEnv = {
+    TEST_MATCH_SUBTEST = "\\.verify-unit-files\\.sh$";
+  };
+}
