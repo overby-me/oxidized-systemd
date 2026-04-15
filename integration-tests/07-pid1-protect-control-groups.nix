2bd6749ef3423376ed3672e45823b2d7096d8e4e
Niclas Overby <niclas@overby.me>
2026-04-15 22:46:56 +0200

    fix(rust/systemd): Fix NixOS PATH issues in clean-unit and protect-control-groups tests
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- integration-tests/07-pid1-protect-control-groups.nix
+++ integration-tests/07-pid1-protect-control-groups.nix
@@ -3,6 +3,9 @@
   testEnv = {
     TEST_MATCH_SUBTEST = "\\.protect-control-groups\\.sh$";
   };
-  patchScript = ''    sed -i '/systemctl --no-block exit 123/d' TEST-07-PID1.sh
+  patchScript = ''
+    sed -i '/systemctl --no-block exit 123/d' TEST-07-PID1.sh
+    # Replace bare 'sh' in ExecStartPost with full NixOS path
+    sed -i "s|ExecStartPost='sh -c|ExecStartPost='/run/current-system/sw/bin/sh -c|g" TEST-07-PID1.protect-control-groups.sh
   '';
 }
--- integration-tests/23-unit-file-clean-unit.nix
+++ integration-tests/23-unit-file-clean-unit.nix
@@ -3,4 +3,9 @@
   testEnv = {
     TEST_MATCH_SUBTEST = "\\.clean-unit\\.sh$";
   };
+  patchScript = ''
+    # Replace bare commands in inline unit files with full NixOS paths
+    sed -i 's|ExecStart=sleep |ExecStart=/run/current-system/sw/bin/sleep |g' TEST-23-UNIT-FILE.clean-unit.sh
+    sed -i 's|ExecStartPre=true|ExecStartPre=/run/current-system/sw/bin/true|g' TEST-23-UNIT-FILE.clean-unit.sh
+  '';
 }
