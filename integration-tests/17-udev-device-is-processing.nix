a42cb31bfabaaad5591420f9b8afef872bfd796b
Niclas Overby <niclas@overby.me>
2026-04-19 19:06:39 +0200

    chore(rust/systemd): Add psmisc to 17-udev-device-is-processing test + statix.toml to silence pre-existing lint
    Signed-off-by: Niclas Overby <niclas@overby.me>

--- integration-tests/17-udev-device-is-processing.nix
+++ integration-tests/17-udev-device-is-processing.nix
@@ -1,5 +1,10 @@
 {
   name = "17-UDEV";
+  # `psmisc` provides `killall`, which the test uses to terminate
+  # the long-running `RUN+=/usr/bin/sleep 1000` udev worker once
+  # ID_PROCESSING=1 has been observed.  Not in the default NixOS
+  # shell environment.
+  extraPackages = pkgs: [pkgs.psmisc];
   testEnv = {
     TEST_MATCH_SUBTEST = "\\.device_is_processing\\.sh$";
   };
